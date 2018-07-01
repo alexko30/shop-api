23d128dbea62006a1152350a99d3399a12212b5e
Alexko30 <rank2505@gmail.com>
2018-07-01 18:49:03 +0300

    Add handling cors errors

--- server/app.js
+++ server/app.js
@@ -7,27 +7,47 @@ import orderRoutes from '../api/routes/orders';
 const app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Constrol-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+
+	if (req.method === 'OPTIONS') {
+		res.header(
+			'Access-Control-Allow-Methods',
+			'PUT, POST, PATCH, DELETE, GET'
+		);
+
+    return res.status(200).json({});
+	}
+
+  next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.statusc= 404;
-  next(error);
-})
+	const error = new Error('Not found');
+	error.statusc = 404;
+	next(error);
+});
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    erros: {
-      message: error.message
-    }
-  })
-})
+	res.status(error.status || 500);
+	res.json({
+		erros: {
+			message: error.message
+		}
+	});
+});
 
 export default app;
